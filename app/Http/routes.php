1c4d0a6433dbcda4e130186375a3bbad626f4c14
Manoj Kumar <nithmanoj@gmail.com>
2016-12-05 15:30:04 +0000

    Added routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,25 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::get('/', 'Helper@DisplayQ');
+
+Route::get('login', function(){
+   return view('login'); 
 });
+
+Route::get('register', function(){
+    return view('register');
+});
+
+Route::get('question/{id}', 'Helper@Question');
+
+Route::get('answer/{id}', 'Helper@Answer');
